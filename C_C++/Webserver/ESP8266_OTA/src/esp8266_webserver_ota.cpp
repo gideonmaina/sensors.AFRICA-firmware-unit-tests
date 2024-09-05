914a5843b0586d1f79848bd78cffacc7f6d14256
gideon maina <gmainapro@yahoo.com>
2024-09-05 17:25:41 +0300

    Implement multiple file uploads

--- C_C++/Webserver/ESP8266_OTA/src/esp8266_webserver_ota.cpp
+++ C_C++/Webserver/ESP8266_OTA/src/esp8266_webserver_ota.cpp
@@ -11,14 +11,16 @@ ESP8266WebServer server(80);
 constexpr unsigned LARGE_STR = 512 - 1;
 constexpr unsigned XLARGE_STR = 1024 - 1;
 
-String loader_checksum, firmware_checksum = "";
+String loader_checksum, firmware_checksum, fname = "";
+bool log_once = true;
 
-File fsUploadFile; // a File object to temporarily store the received file
+File uploadFile; // a File object to temporarily store the received file
 void log_args();
 void uploadFiles();
 static void webserver_ota_begin();
 static void webserver_ota_upload();
 static void setup_webserver();
+static void file_checker();
 
 #define RESERVE_STRING(name, size)      \
     String name((const char *)nullptr); \
@@ -96,22 +98,20 @@ static void end_html_page(String &page_content)
 static void setup_webserver()
 {
     server.on(F("/ota_upload"), webserver_ota_upload);
-    server.on(F("/ota_begin"), HTTP_POST, webserver_ota_begin);
+    server.on(F("/ota_begin"), HTTP_POST, []()
+              { 
+                // server.sendHeader(F("Transfer-Encoding"), F("chunked"));
+                server.send(200); }, // Send status 200 (OK) to tell the client we are ready to receive
+              webserver_ota_begin);
 
-    Serial.print("Starting Webserver... "),
-        Serial.println(WiFi.localIP().toString());
+    Serial.print("Starting Webserver... ");
     server.begin();
 }
 
 static void webserver_ota_upload()
 {
 
-    server.sendHeader(F("Cache-Control"), F("no-cache, no-store, must-revalidate"));
-    server.sendHeader(F("Pragma"), F("no-cache"));
-    server.sendHeader(F("Expires"), F("0"));
-
     RESERVE_STRING(page_content, XLARGE_STR);
-    // start_html_page(page_content, emptyString);
     start_html_page(page_content, "OTA update");
     page_content += "<br/><br/>";
     page_content += F("<form method='POST' action='/ota_begin' enctype='multipart/form-data' style='width:100%;'>\n<b> OTA OVER ESP AP WEBSERVER </b><br/>");
@@ -132,69 +132,88 @@ static void webserver_ota_upload()
 
 static void webserver_ota_begin()
 {
-    // Debuging: Log args
-    log_args();
 
-    // get server post arguements
-    if (!server.hasArg("loader_checksum"))
+    if (server.args() > 0)
     {
-        Serial.println("Loader bin MD5 checksum missing");
-        return;
-    }
-    loader_checksum = server.arg("loader_checksum");
-    if (!server.hasArg("fmw_checksum"))
-    {
-        Serial.println("Firmware bin MD5 checksum missing");
-        return;
-    }
-    firmware_checksum = server.arg("fmw_checksum");
+        // Debuging: Log args
+        log_args();
 
-    // upload files
+        // get server post arguements
+        if (!server.hasArg("loader_checksum"))
+        {
+            Serial.println("Loader bin MD5 checksum missing");
+            return;
+        }
+        loader_checksum = server.arg("loader_checksum");
+        if (!server.hasArg("fmw_checksum"))
+        {
+            Serial.println("Firmware bin MD5 checksum missing");
+            return;
+        }
+        firmware_checksum = server.arg("fmw_checksum");
+    }
+
+    // uploadFiles();
     uploadFiles();
+
     // validate md5
-    // beging upload
+
+    // begin upload
 }
 
 void uploadFiles()
-{ // upload a new file to the SPIFFS
+{
+    // upload a new file to the SPIFFS
     HTTPUpload &upload = server.upload();
 
-    while (upload.status == UPLOAD_FILE_START)
+    Serial.print("Server upload status: ");
+    Serial.println(upload.status);
+
+    if (upload.status == UPLOAD_FILE_START)
     {
 
-        String fname = upload.filename;
+        fname = upload.filename;
         if (!fname.startsWith("/"))
             fname = "/" + fname;
         Serial.print("Upload File Name: ");
         Serial.println(fname);
-        fsUploadFile = SPIFFS.open(fname, "w"); // Open the file for writing in SPIFFS (create if it doesn't exist)
+        uploadFile = SPIFFS.open(fname, "w"); // Open the file for writing in SPIFFS (create if it doesn't exist)
+        if (uploadFile)
+        {
+            Serial.println("File opened");
+        }
         fname = String();
-
-        if (upload.status == UPLOAD_FILE_WRITE)
+        Serial.print("fname: ");
+        Serial.println(fname);
+    }
+    else if (upload.status == UPLOAD_FILE_WRITE)
+    {
+        if (uploadFile)
         {
-            if (fsUploadFile)
-                fsUploadFile.write(upload.buf, upload.currentSize);
+            uploadFile.write(upload.buf, upload.currentSize);
+            Serial.println("written");
         }
+    }
 
-        else if (upload.status == UPLOAD_FILE_END)
-        {
-            if (fsUploadFile)
-            {                         // If the file was successfully created
-                fsUploadFile.close(); // Close the file again
-                Serial.print("File Upload Size: ");
-                Serial.println(upload.totalSize);
-                String msg = "201: Successfully created file ";
-                msg += fname;
-                server.send(500, "text/plain", msg);
-            }
-            else
-            {
-                String err_msg = "500: failed creating file ";
-                err_msg += fname;
-                server.send(500, "text/plain", err_msg);
-            }
+    else if (upload.status == UPLOAD_FILE_END)
+    {
+        if (uploadFile)
+        {                       // If the file was successfully created
+            uploadFile.close(); // Close the file again
+            Serial.print("File Upload Size: ");
+            Serial.println(upload.totalSize);
+            String msg = "201: Successfully created file ";
+            msg += fname;
+            server.send(200, "text/plain", msg);
+            Serial.println(msg);
         }
-        delay(1000);
+        else
+        {
+            String err_msg = "500: failed creating file ";
+            err_msg += fname;
+            server.send(500, "text/plain", err_msg);
+            Serial.println(err_msg);
+        }
     }
 }
 
@@ -215,20 +234,53 @@ void log_args()
     }
 }
 
+static void file_checker()
+{
+    File loaderFile = SPIFFS.open(F("/loader-002.bin"), "r");
+    if (!loaderFile)
+    {
+        Serial.println("loader file does not exist");
+    }
+    else
+    {
+
+        Serial.println("loader file present");
+        loaderFile.close();
+    }
+
+    File fmwFile = SPIFFS.open(F("/new_firmware.bin"), "r");
+
+    if (!fmwFile)
+    {
+        Serial.println("firmware file does not exist");
+    }
+    else
+    {
+
+        Serial.println("firmware file present");
+        fmwFile.close();
+    }
+}
+
 void setup()
 {
+    delay(1000);
     Serial.begin(9600);
     delay(2000);
     const char *ssid = "ESP8266-OTA-TEST";
     const char *password = "123456789";
     WiFi.mode(WIFI_AP);
     const IPAddress apIP(192, 168, 4, 1);
-    WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
     WiFi.softAP(ssid, password);
+    WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
     delay(2000);
     setup_webserver();
+    SPIFFS.begin();
+    delay(2000);
+    file_checker(); // Reset after successful file upload to view saved files
 }
 
 void loop()
 {
+    server.handleClient();
 }
